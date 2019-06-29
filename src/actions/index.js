bdfc86ce22df0fb972c74976510ae87dc256bb14
Alejandro Mansor <amansor@gmail.com>
2019-06-29 16:45:02 -0300

    RedditList component connected to Redux - transformed to class component to apply lifecycle methods

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,13 +2,13 @@ import {FETCH_POST, FETCH_POSTS} from './types';
 import posts from '../apis/posts';
 
 export const fetchPosts = () => async dispatch => {
- const response = await streams.get('/posts');
+ const response = await posts.get('/posts');
 
  dispatch({type: FETCH_POSTS, payload: response.data});
 }
 
 export const fetchPost = (id) => async dispatch => {
-  const response = await streams.get(`streams/${id}`);
+  const response = await posts.get(`posts/${id}`);
 
   dispatch({type: FETCH_POST, payload: response.data});
 }
--- src/components/RedditList.js
+++ src/components/RedditList.js
@@ -1,13 +1,25 @@
 import React from 'react';
+import {connect} from 'react-redux';
+import {fetchPosts} from '../actions';
+
 import RedditListItem from './RedditListItem';
 
-const RedditList = () => {  
-  return (
-    <div className="ui relaxed divided list">
-      <h3>Reddit Posts</h3>
-      <RedditListItem />
-    </div>
-  )
+class RedditList extends React.Component {    
+
+  render(){
+    return (
+      <div className="ui relaxed divided list">
+        <h3>Reddit Posts</h3>
+        <RedditListItem />
+      </div>
+    )
+  }
 }
 
-export default RedditList;
+const mapStateToProps = (state) => {
+  return {
+    posts: Object.values(state.posts),
+  };
+}
+
+export default connect(mapStateToProps, {fetchPosts}) (RedditList);
