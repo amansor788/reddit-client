056d507e0920f48c27dc8c50bdd6cd741eb57f5a
Alejandro Mansor <amansor@gmail.com>
2019-07-01 20:07:56 -0300

    Updates

---
+++ precache-manifest.00fe84b0f109c82fccd771ef02347a61.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30c8c7706c56f08d3ca8608ac5c81fc6",
+    "url": "/reddit-client/index.html"
+  },
+  {
+    "revision": "7de55fd881c560f98d0b",
+    "url": "/reddit-client/static/css/main.b1670d07.chunk.css"
+  },
+  {
+    "revision": "93900a4ec7088716323e",
+    "url": "/reddit-client/static/js/2.67ab6038.chunk.js"
+  },
+  {
+    "revision": "7de55fd881c560f98d0b",
+    "url": "/reddit-client/static/js/main.e60c8944.chunk.js"
+  },
+  {
+    "revision": "df39fec3b7a641d36a27",
+    "url": "/reddit-client/static/js/runtime~main.384cf613.js"
+  }
+]);
