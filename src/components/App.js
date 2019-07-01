04c1b2bdbd0aaaf8eefb2113789649e3bf2ba61b
Alejandro Mansor <amansor@gmail.com>
2019-07-01 20:17:54 -0300

    webpack deploy configuration to GitHub Pages: https://amansor788.github.io/reddit-client/

--- src/components/App.js
+++ src/components/App.js
@@ -7,10 +7,10 @@ class App extends React.Component {
   render(){
     return (
         <div className="ui padded grid cust">
-            <div className="seven wide column cust">
+            <div className="six wide column cust">
               <RedditList />
             </div>
-            <div className="nine wide column cust">
+            <div className="ten wide column cust">
               <RedditDetail />
             </div>
         </div>
